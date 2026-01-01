4baf314337acdcc6fcc0550559e310d192b82ad8
fedorkobak <kobfedsur@gmail.com>
2026-01-02 00:40:45 +0300

    add files tree to the nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,7 @@
+vim.cmd("set expandtab")
 vim.opt.clipboard = 'unnamedplus'
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 
 -- Plugins
 require('config.lazy')
@@ -8,3 +11,5 @@ vim.cmd.colorscheme "catppuccin"
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+
+vim.keymap.set('n', '<C-b>', ":Neotree filesystem reveal left<CR>", {})
--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -23,15 +23,25 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-      { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-      {
-          'nvim-telescope/telescope.nvim',
-           dependencies = {
-              'nvim-lua/plenary.nvim',
-              { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-           }
-      }
+    spec = {
+        { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+        {
+            'nvim-telescope/telescope.nvim',
+            dependencies = {
+                'nvim-lua/plenary.nvim',
+                { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+            }
+        },
+        {
+            "nvim-neo-tree/neo-tree.nvim",
+            branch = "v3.x",
+            dependencies = {
+                "nvim-lua/plenary.nvim",
+                "MunifTanjim/nui.nvim",
+                "nvim-tree/nvim-web-devicons",
+            },
+            lazy = false,
+        }
     },
     install = { },
     checker = { enabled = true },
