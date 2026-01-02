5b94562d3c12ba24c928b01d5cc8c7a3f68b97b2
fedorkobak <kobfedsur@gmail.com>
2026-01-02 12:41:49 +0300

    add linenumber to nvim configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,8 @@ vim.cmd("set expandtab")
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- Plugins
 require('config.lazy')
