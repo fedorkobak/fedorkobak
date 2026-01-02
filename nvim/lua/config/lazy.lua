8c8598a073b6248585b3888237a4ba6fd9002985
fedorkobak <kobfedsur@gmail.com>
2026-01-02 12:49:34 +0300

    move plugins to the special file

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -22,28 +22,5 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
-require("lazy").setup({
-    spec = {
-        { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-        {
-            'nvim-telescope/telescope.nvim',
-            dependencies = {
-                'nvim-lua/plenary.nvim',
-                { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-            }
-        },
-        {
-            "nvim-neo-tree/neo-tree.nvim",
-            branch = "v3.x",
-            dependencies = {
-                "nvim-lua/plenary.nvim",
-                "MunifTanjim/nui.nvim",
-                "nvim-tree/nvim-web-devicons",
-            },
-            lazy = false,
-        }
-    },
-    install = { },
-    checker = { enabled = true },
-})
+require("lazy").setup("plugins")
 
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,20 @@
+return {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        }
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        lazy = false,
+    }
+}
