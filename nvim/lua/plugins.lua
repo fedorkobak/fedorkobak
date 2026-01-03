81e856176ae558a168b0a7633c754ba65c600558
fedorkobak <kobfedsur@gmail.com>
2026-01-03 05:03:08 +0300

    update LSP configuration

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -30,5 +30,43 @@ return {
         config = function()
             vim.keymap.set('n', '<C-b>', ":Neotree filesystem reveal left<CR>", {})
         end
+    },
+    {
+        "mason-org/mason.nvim",
+        opts = {}
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {},
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "pyright" }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+        end
+    },
+    {
+        "mfussenegger/nvim-lint",
+        config = function()
+            local lint = require("lint")
+            lint.linters_by_ft = {
+                python = { "flake8" }
+            }
+
+            vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+              callback = function()
+                lint.try_lint()
+              end,
+            })
+        end
     }
 }
