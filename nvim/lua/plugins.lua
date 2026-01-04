0d46c715fec7464bf0d1da0974f9fce0c64966d2
fedorkobak <kobfedsur@gmail.com>
2026-01-04 23:24:53 +0300

    add none-ls configuration

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,72 +1,90 @@
 return {
-    { 
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme "catppuccin"
-        end
-    },
-    {
-        'nvim-telescope/telescope.nvim',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-        },
-        config = function()
-            local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-        end
-    },
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "MunifTanjim/nui.nvim",
-            "nvim-tree/nvim-web-devicons",
-        },
-        lazy = false,
-        config = function()
-            vim.keymap.set('n', '<C-b>', ":Neotree filesystem reveal left<CR>", {})
-        end
-    },
-    {
-        "mason-org/mason.nvim",
-        opts = {}
-    },
-    {
-        "mason-org/mason-lspconfig.nvim",
-        opts = {},
-        dependencies = {
-            { "mason-org/mason.nvim", opts = {} },
-            "neovim/nvim-lspconfig",
-        },
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pyright" }
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-        end
-    },
-    {
-        "mfussenegger/nvim-lint",
-        config = function()
-            local lint = require("lint")
-            lint.linters_by_ft = {
-                python = { "flake8" }
-            }
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+		end,
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		lazy = false,
+		config = function()
+			vim.keymap.set("n", "<C-b>", ":Neotree filesystem reveal left<CR>", {})
+		end,
+	},
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "pyright" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+		end,
+	},
+	{
+		"mfussenegger/nvim-lint",
+		config = function()
+			local lint = require("lint")
+			lint.linters_by_ft = {
+				python = { "flake8" },
+			}
 
-            vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-              callback = function()
-                lint.try_lint()
-              end,
-            })
-        end
-    }
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+		end,
+	},
+	{
+		"nvimtools/none-ls-extras.nvim",
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.completion.spell,
+					require("none-ls.diagnostics.eslint"),
+				},
+			})
+			vim.keymap.set("n", "<C-g>", vim.lsp.buf.format, {})
+		end,
+	},
 }
