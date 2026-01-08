406c4aa8fa52d2e294da4deb9ca98ed83fc86c5a
fedorkobak <kobfedsur@gmail.com>
2026-01-08 22:51:19 +0300

    add autocomplition

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -31,50 +31,29 @@ return {
 			vim.keymap.set("n", "<C-b>", ":Neotree filesystem reveal left<CR>", {})
 		end,
 	},
-	{
-		"mason-org/mason.nvim",
-		opts = {},
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
+	{"L3MON4D3/LuaSnip",},
+	{"hrsh7th/nvim-cmp"},
+    {
+        "hrsh7th/cmp-nvim-lsp",
+        config = function()
+            local cmp = require("cmp")
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					-- { name = "nvim_lsp" },
-					-- { name = "vsnip" }, -- For vsnip users.
-					{ name = "luasnip" }, -- For luasnip users.
-					-- { name = 'ultisnips' }, -- For ultisnips users.
-					-- { name = 'snippy' }, -- For snippy users.
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
+            cmp.setup({
+              completion = {
+                autocomplete = { cmp.TriggerEvent.TextChanged },
+              },
+
+              mapping = cmp.mapping.preset.insert({
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<Tab>"] = cmp.mapping.select_next_item(),
+                ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+              }),
+
+              sources = {
+                { name = "nvim_lsp" },
+              },
+            })
+        end,
+    },
 }
