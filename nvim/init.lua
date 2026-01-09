cc6daeb7f74323398d024c42d72d73d647dde902
fedorkobak <kobfedsur@gmail.com>
2026-01-09 10:57:27 +0100

    add eng<->rus keymap

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,7 @@ vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.langmap = "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
 
 -- Plugins
 require('config.lazy')
