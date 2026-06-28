65f3e54a9ad60fbd42aa46bf980d27857a180557
fedorkobak <kobfedsur@gmail.com>
2026-06-28 14:38:43 +0300

    add molten to configuration

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -107,4 +107,12 @@ return {
 
         end,
     },
+    {
+        "benlubas/molten-nvim",
+        version = "^1.0.0",
+        build = ":UpdateRemotePlugins",
+        init = function()
+            vim.g.molten_output_win_max_height = 12
+        end,
+    },
 }
